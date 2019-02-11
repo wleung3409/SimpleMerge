78f644bdf68806d34be731f5b920b24b2dba1d84
William <wleung3409@bths.edu>
2019-02-10 19:45:35 -0500

    done

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -4,9 +4,30 @@
  * integers and merges them to return one sorted
  * array of integers.
  */
-public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
-
+public class SimpleMerge
+{
+    public static int[] simpleMerge(int[] arr1, int[] arr2)
+    {
+        int[] array = new int[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++)
+        {
+            array[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++)
+        {
+            array[arr1.length + i] = arr2[i];
+        }
+        for (int i = 0; i < array.length; i++)
+        {
+            int number = array[i];
+            int j;
+            for (j = i - 1; j >= 0 && number < array[j]; j--)
+            {
+                array[j + 1] = array[j];
+            }
+            array[j + 1] = number;
+        }
+        return array;
     }
-
 }
+
